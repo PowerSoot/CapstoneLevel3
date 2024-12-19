470e16c3aad486d2966aae872e7d53eaa66cf75c
Globull Broad Stratocrashster <powersoot@outlook.com>
2024-12-18 22:02:38 -0600

    Delete modules directory

--- modules/Qoth/extractQotdResponse.js
+++
@@ -1,7 +0,0 @@
-import { Qotd } from "./Qotd";
-
-//Extract the server resoponse and parse it
-export function extractQotdResponse(resolveValue) {
-    const promise = resolveValue.text();
-    promise.then();
-}
--- modules/Qoth/parseQotdResponse.js
+++
@@ -1,9 +0,0 @@
-export function parseQotdResponse(resolveValue) {
-    debugger;
-    const response = JSON.parse(resolveValue);
-    const results =  response.quote;
-    const quote = results.body;
-    const author= results.author;
-
-    return `${quote}<br> - ${author}`;
-    }
